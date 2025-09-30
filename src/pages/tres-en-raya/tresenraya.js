3457aef3c713334d7e904aa7b01a76b3744d8b2d
Fernando Developer <fernandocodigo52@gmail.com>
2025-09-30 12:16:16 +0200

    agregar estructura y estilos para el título en el juego "Tres en Raya"

--- src/pages/tres-en-raya/tresenraya.js
+++ src/pages/tres-en-raya/tresenraya.js
@@ -3,5 +3,13 @@ import "./tresenraya.css";
 export const initTresEnRaya = (divApp) =>{
   const divPantallaTres = document.createElement("div")
   divPantallaTres.className = "divPantallaTres"
+  const divTitulo = document.createElement("div")
+  divTitulo.className = "div-titulo-tres"
+  const titulo = document.createElement("h2")
+  titulo.textContent = "TRES EN RAYA"
+  
+  
+  divTitulo.append(titulo)
+  divPantallaTres.append(divTitulo)
   divApp.append(divPantallaTres)
 }
